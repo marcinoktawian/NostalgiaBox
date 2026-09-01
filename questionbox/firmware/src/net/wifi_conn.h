4fef6828ce26fcaa92ab2ac00d0c8c1b3520d75b
szatko <szatko@dailynux.dailynux>
2026-09-01 11:12:09 +0200

    Delete questionbox

--- questionbox/firmware/src/net/wifi_conn.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-// Connects to the Wi-Fi configured in secrets.h. Blocks up to timeout_ms.
-// Returns true if connected. The ESP32-S3 is 2.4GHz only.
-bool WiFiConn_Connect(uint32_t timeout_ms = 20000);
-
-bool WiFiConn_IsConnected();
